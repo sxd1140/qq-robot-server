8b7fbf51026fc29c6fb828fde504727e23a7ce4f
sxd <sxd1140@aliyun.com>
2021-06-17 00:34:06 +0800

    准备提供httpserver

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:puppeteer/plugins/stealth.dart';
 import 'package:puppeteer/puppeteer.dart';
 import 'package:qq_robot_server/manager/QQManager.dart';
 
+import 'httpServer.dart' as httpServer;
+
 void main(List<String> arguments) {
-  QQManager().init();
   puppeteer.plugins.add(StealthPlugin());
+  QQManager().init();
+  httpServer.init();
 }
